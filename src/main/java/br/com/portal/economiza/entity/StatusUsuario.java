47ee8eeeb1caa1619e043e5f88d118ad29197082
Diogo Humberto <diogo_humberto@hotmail.com>
2022-01-16 15:16:18 -0300

    inicio do projeto - portal - autenticacao

---
+++ src/main/java/br/com/portal/economiza/entity/StatusUsuario.java
@@ -0,0 +1,10 @@
+package br.com.portal.economiza.entity;
+
+public enum StatusUsuario {
+	
+	ATIVO,
+	INATIVO,
+	CANCELADO
+	
+
+}
---
+++ src/test/java/br/com/cassol/marketplaces/MarketplacesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cassol.marketplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
